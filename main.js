9ba4e03eac0592582df5822410d8d90d5b7f9e33
yifu.wang <yifu.wang@zenlayer.com>
2026-04-27 11:43:54 +0800

    feat: migrate from Lingva TTS to Mimo TTS plugin

--- main.js
+++ main.js
@@ -1,23 +1,64 @@
 async function tts(text, lang, options = {}) {
-    const { config, utils } = options;
-    const { tauriFetch } = utils;
-    let { requestPath } = config;
-    if (requestPath === undefined || requestPath.length === 0) {
-        requestPath = "lingva.pot-app.com"
-    }
-    if (!requestPath.startsWith('http')) {
-        requestPath = 'https://' + requestPath;
-    }
-    const res = await tauriFetch(`${requestPath}/api/v1/audio/${lang}/${encodeURIComponent(text)}`);
+  const { config, utils } = options;
+  const { http } = utils;
+  const { fetch, Body } = http;
+  let { requestPath, apiKey, model, voice, voiceStyle } = config;
 
-    if (res.ok) {
-        let result = res.data;
-        if (result['audio']) {
-            return result['audio'];
-        } else {
-            throw JSON.stringify(result);
-        }
+  if (!requestPath) {
+    requestPath = "https://api.xiaomimimo.com/v1/chat/completions";
+  }
+  if (!/https?:\/\/.+/.test(requestPath)) {
+    requestPath = `https://${requestPath}`;
+  }
+  if (requestPath.endsWith("/")) {
+    requestPath = requestPath.slice(0, -1);
+  }
+
+  if (!apiKey) {
+    throw "apiKey is required";
+  }
+  if (!model) {
+    model = "mimo-v2.5-tts";
+  }
+  if (!voice) {
+    voice = "Mia";
+  }
+
+  const res = await fetch(requestPath, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "api-key": apiKey,
+    },
+    body: Body.json({
+      model,
+      audio: {
+        format: "wav",
+        voice,
+      },
+      messages: [
+        {
+          role: "user",
+          content: voiceStyle,
+        },
+        {
+          role: "assistant",
+          content: text,
+        },
+      ],
+    }),
+    responseType: 3,
+  });
+  if (res.ok) {
+    let result = res.data;
+    if (result["audio"]) {
+      return result["audio"];
     } else {
-        throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(res.data)}`;
+      throw JSON.stringify(result);
     }
+  } else {
+    throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(
+      res.data
+    )}`;
+  }
 }
